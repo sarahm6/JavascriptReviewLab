52e3f8fac4b5c5e20e2f204e20b6951707a10654
Sarah Memon <sarahmemon@gmail.com>
2022-12-14 13:49:40 -0500

    Objects complete

--- JavascriptReviewLab.js
+++ JavascriptReviewLab.js
@@ -386,6 +386,105 @@ const printLongestWord = (array) => {
   console.log(printLongestWord(["BoJack", "Princess", "Diane", "a", "Max", "Peanutbutter", "big", "Todd"]));
   
 
+// Objects
+// Let's set up an object data structure. Let's say we have a website that sells products, and we have a user of our website, and we want to store that user's data. The object data structure is a good way to organize the data from our user.
+
+// A. Make a user object
+// Create an object called user.
+// Write in to the object the key-value pairs for name, email, age, and purchased. Set the value of purchasedto an empty array []. Set the other values to whatever you would like.
+const user = {
+    name: "Zoha",
+    email: "zoha@gmail.com",
+    age: 12,
+    purchased: []
+  }
+  console.log(user)
+
+// B. Update the user
+// Our user has changed his or her email address. Without changing the original userobject, update the emailvalue to a new email address.
+user.email = "zohaha@gmail.com";
+console.log(user)
+
+// Our user has had a birthday! Without changing the original userobject, increment the agevalue using the postfix operator. Hint: age++
+user.age = ++user.age
+console.log(user.age)
+
+// C. Adding keys and values
+// You have decided to add your user's location to the data that you want to collect.
+// Without changing the original userobject, add a new key locationto the object, and give it a value or some-or-other location (a string).
+user.location = "Virginia"
+console.log(user.location)
+
+// D. Shopaholic!
+// 1. Our user has purchased an item! They have purchased some "carbohydrates". Using .push(), add the string "carbohydrates" to the purchasedarray.
+user.purchased.push("carbohydrates")
+console.log(user.purchased)
+// 2. Our user has purchased an item! They have purchased some "peace of mind". Using .push(), add the string "peace of mind" to the purchasedarray.
+user.purchased.push("peace of mind")
+console.log(user.purchased)
+// 3. Our user has purchased an item! They have purchased some "Merino jodhpurs". Using .push(), add the string "Merino jodhpurs" to the purchasedarray.
+user.purchased.push("merino jodhpurs")
+console.log(user.purchased)
+// 4. Console.log just the "Merino jodhpurs" from the purchasedarray.
+console.log(user.purchased[2])
+
+// E. Object-within-object
+// Write a friendobject into your userobject and give the friend a name, age, location, and purchased array (empty for now)
+user.friend = {name:"Kramer", age: 40, location:"New York City", purchased: []}
+// Console.log just the friend's name
+console.log(user.friend.name)
+// Console.log just the friend's location
+console.log(user.friend.location)
+// CHANGE the friend's age to 55
+user.friend.age = 55
+console.log(user.friend.age)
+// The friendhas purchased "The One Ring". Use .push()to add "The One Ring" to the friend's purchasedarray.
+user.friend.purchased.push("The One Ring")
+console.log(user.friend.purchased)
+// The friendhas purchased "A latte". Use .push()to add "A latte" to the friend's purchasedarray.
+user.friend.purchased.push("A latte")
+console.log(user.friend.purchased)
+// Console.log just "A latte" from the friend's purchasedarray.
+console.log(user.friend.purchased[1])
+
+// F. Loops
+// Write a for loop that iterates over the User's purchasedarray (NOT the friend's purchased array), and prints each element to the console.
+for (let i=0; i < user.purchased.length; i++){
+    console.log(user.purchased[i])
+}
+// Write a for loop that iterates over the Friend's purchasedarray, and prints each element to the console.
+for (let i=0; i < user.friend.purchased.length; i++){
+    console.log(user.friend.purchased[i])
+}
 
 
-
+// G. Functions can operate on objects
+// Write a single function updateUserthat takes no parameters. When the function is run, it should:
+// it should increment the user's age by 1
+// make the user's name uppercase
+// The function does not need a returnstatement, it will merely modify the user object.
+updateUser = () => {
+    if (user.age == user.age) {
+      ++user.age
+    } else if (user.name == user.name) {
+      user.name.toUpperCase
+    }
+    user.name = user.name.toUpperCase();
+  console.log(user)
+  }
+  updateUser()
+  
+  // Write a function oldAndLoudthat performs the exact same tasks as updateUser, but instead of hard-coding it to only work on our userobject, make it take a parameter person, and have it modify the object that is passed in as an argument when the function is called. Call your oldAndLoudfunction with useras the argument.
+  var person
+  oldAndLoud = (person) => {
+    if (person.name == user.name || user.friend.name) {
+      person.name.toUpperCase()
+    }
+    else if (person.age == user.age || user.friend.age) {
+      ++person.age
+    }
+    console.log(person.name)
+    console.log(person.age)
+  }
+  
+  oldAndLoud((user))
